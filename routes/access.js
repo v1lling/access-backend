2df7d35346e0cb471deb413b3f4cf6b82f5690cc
v1lling <sascha.villing@web.de>
2021-10-28 14:58:03 +0200

    Change back route

--- routes/access.js
+++ routes/access.js
@@ -3,7 +3,7 @@ var checkinService = require('../services/checkinService');
 
 var router = express.Router();
 
-router.get('/checkin', async (req, res) => {
+router.post('/checkin', async (req, res) => {
   console.log(req.body);
 // playlist = await checkinService.checkin(req.body.userinfo);
   res.sendStatus(200);
