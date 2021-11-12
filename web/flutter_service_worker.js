1dacd9518b92af971dd39fe2010898db713fce4f
v1lling <sascha.villing@web.de>
2021-11-12 10:57:47 +0100

    Build webclient

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -41,9 +41,9 @@ const RESOURCES = {
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "version.json": "b436de0ae8478d892e8d0fd86680fa1a",
-"main.dart.js": "3493cd32d4ec78cfc4d96e1a2c2982f3",
-"index.html": "03409e934cb9c3c6df59aca998446c9e",
-"/": "03409e934cb9c3c6df59aca998446c9e"
+"main.dart.js": "8601ca597179faafbe893993a5616fdf",
+"index.html": "115cfa5437f3bfaa9690d1c9eec18b8d",
+"/": "115cfa5437f3bfaa9690d1c9eec18b8d"
 };
 
 // The application shell files that are downloaded before a service worker can
