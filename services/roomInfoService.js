dc2f1495d216b2b2eb4f257e1918b12964530bcf
v1lling <sascha.villing@web.de>
2021-11-12 13:28:26 +0100

    Fix bug

--- services/roomInfoService.js
+++ services/roomInfoService.js
@@ -1,9 +1,7 @@
 const getCurrentPeopleCount =  function(roomId) {
     return new Promise(async (resolve, reject) => {
        
-        var peoplecount = await accessDb.collection('checkins').find({checkin: {$lt: new Date()},checkout: { $gte: new Date()}}).toArray();
-        console.log(peoplecount);
-        // > db.checkins.findOne({checkin: {$lt: new Date()},checkout: { $gte: new Date()}})
+        var peoplecount = await accessDb.collection('checkins').find({roomId: roomId, checkin: {$lt: new Date()},checkout: { $gte: new Date()}}).toArray();
         resolve(peoplecount.length);
     });
 }
