883b6283624bcb7f481cc02360eeeaa258682a4c
v1lling <sascha.villing@web.de>
2021-11-26 15:49:42 +0100

    Build webclient

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -36,15 +36,15 @@ const RESOURCES = {
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/flutter_nfc_web/assets/flutter_nfc.js": "dadf28f15920e11f7415c3249143b229",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"manifest.json": "7200f8541bef3b3ac755f1660c24bad8",
+"manifest.json": "6bd3aa0db89f8d04fe4ad9607c29fbc8",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "version.json": "b436de0ae8478d892e8d0fd86680fa1a",
-"main.dart.js": "05d6ccc4bec881feae5900b6564c03cd",
-"index.html": "5132fe77dea42f85122a8f55f3fbb98d",
-"/": "5132fe77dea42f85122a8f55f3fbb98d"
+"main.dart.js": "fcd8a539f3662b19b4910e5e7746582e",
+"index.html": "7fe890fbe15d4e0bf896fff9de2aecf3",
+"/": "7fe890fbe15d4e0bf896fff9de2aecf3"
 };
 
 // The application shell files that are downloaded before a service worker can
